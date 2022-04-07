fcfa12ef7404b74e8e95544822b1c8c2909ddb62
Jānis Alunāns <j.alunans@gmail.com>
2022-04-07 23:33:29 +0300

    Installed application gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :twits
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "twits#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
